317b22d9328ed28db552032dfaff4a3636462f5b
Jan Paul Ettles <janpaulettles@gmail.com>
2013-01-19 21:05:53 +0000

    removed comments

--- src/test/java/jan/mongometer/QuickEnvironment.java
+++ src/test/java/jan/mongometer/QuickEnvironment.java
@@ -28,26 +28,5 @@ public class QuickEnvironment {
 
         String[] mongo = {"bash", "-c", "xterm -e 'cd /usr/lib/mongodb/2.0.6/bin/; ./mongo; bash'"};
         Runtime.getRuntime().exec(mongo);
-
-
-        //Runtime.getRuntime().exec("gnome-terminal --geometry=100x40 --working-directory=/usr/lib/mongodb/2.0.6/bin/ -x bash -c \"ls; cat\"");
-        //Runtime.getRuntime().exec("/usr/bin/xterm -e /usr/lib/mongodb/2.0.6/bin/");
-        //Runtime.getRuntime().exec("xterm -e 'cd /usr/lib/mongodb/2.0.6/bin/; ./mongo; bash'");
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Development\\mongodb-win32-i386-2.0.2-rc1\\bin && mongod --repair\"");
-        //Thread.sleep(10000);
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Development\\mongodb-win32-i386-2.0.2-rc1\\bin && mongod --replSet modset --keyFile modkey --maxConns 200\"");
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Development\\mongodb-win32-i386-2.0.2-rc1\\bin && mongod --replSet modset --keyFile modkey\"");
-        //Thread.sleep(10000);
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Development\\burp && java -version:\"1.6\" -jar burpsuite_pro_v1.4.jar\"");
-        //Thread.sleep(1000);
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Development\\mongodb-win32-i386-2.0.2-rc1\\bin && mongo landing -u landinguser -p landingpass\"");
-        //Thread.sleep(1000);
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Development\\apache-tomcat-6.0.33\\bin && startup.bat\"");
-        //Thread.sleep(1000);
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Development\\apache-jmeter-2.6\\bin && jmeter.bat\"");
-        //Thread.sleep(1000);
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Development\\jvmstat\\bat && jvmstat\"");
-        //Thread.sleep(1000);
-        //Runtime.getRuntime().exec("cmd /K start cmd.exe /K \"cd C:\\Users\\ettlesj && runevery.bat -p mongo -s 2\"");
-    }
+   }
 }
